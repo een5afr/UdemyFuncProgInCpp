7a4f04764188820d0c3f5e848cfe5ab2d523e738
AhmedR_Work_WSL <een5afr@googlemail.com>
2022-10-01 19:41:50 +0100

    Added a WIP cc file and compile script

---
+++ compile.sh
@@ -0,0 +1,3 @@
+rm -rf ./run
+g++ -std=c++14 -Wall wip.cc -o run
+./run
---
+++ wip.cc
@@ -0,0 +1,31 @@
+#include <fplus/fplus.hpp>
+#include <iostream>
+
+struct cat
+{
+    double cuteness() const
+    {
+        return softness_ * temperature_ * roundness_ * fur_amount_ - size_;
+    }
+    std::string name_;
+    double softness_;
+    double temperature_;
+    double size_;
+    double roundness_;
+    double fur_amount_;
+};
+
+int main()
+{
+    std::vector<cat> cats = {
+        {"Tigger",   5, 5, 5, 5, 5},
+        {"Simba",    2, 9, 9, 2, 7},
+        {"Muffin",   9, 4, 2, 8, 6},
+        {"Garfield", 6, 5, 7, 9, 5}};
+
+    auto cutest_cat = fplus::maximum_on(std::mem_fn(&cat::cuteness), cats);
+
+    std::cout << cutest_cat.name_ <<
+        " is happy and sleepy. *purr* *purr* *purr*" << std::endl;
+    return 0;
+}
